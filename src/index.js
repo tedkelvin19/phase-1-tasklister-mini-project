4037dad11cc6b93d584af64fe5850672302532e6
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-10-01 22:10:02 +0300

    Finished assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    buildNewtask(e.target.todoList.value)
+    form.reset()
+  })
 });
+function buildNewtask(todo){
+ let listElement = document.createElement("li")
+ let btn  = document.createElement("button")
+ btn.textContent = "X"
+ listElement.textContent = `${todo}  `
+ listElement.appendChild(btn) 
+console.log(listElement)
+ document.querySelector('#tasks').appendChild(listElement)
+ btn.addEventListener('click',handleToDoList)
+}
+function  handleToDoList(e){
+  e.target.parentNode.remove()
+}
